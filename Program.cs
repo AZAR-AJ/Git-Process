57e148b20b2b73eb16b1f175aef5d782cc600f46
Azar J <azaarjam07@outlook.com>
2021-09-04 12:40:09 +0530

    Change in console print

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Git_Process
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Hi This is my first change !");
         }
     }
 }
